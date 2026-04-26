c2c5eeb28ced76cb678f05d775a747afa9846d95
Mahmood Saadalla <mahmoodsadalla@gmail.com>
2026-04-26 18:34:53 +0300

    Add project files.

---
+++ AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_12_EF02
+{
+    internal class AppDbContext:DbContext
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=. ; Database=MyDatabase ; Trusted_Connection=True ; trustServerCertificate=True");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_12_EF02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDbContext Db = new AppDbContext();
+        }
+    }
+}
